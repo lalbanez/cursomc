a22317c691785dc92932f2578736697d99755454
Leandro Rezende Albanez <leandrorezende89@gmail.com>
2025-01-05 15:09:19 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/leandroalbanez/cursomc/services/CategoriaService.java
+++ src/main/java/com/leandroalbanez/cursomc/services/CategoriaService.java
@@ -6,6 +6,9 @@ import com.leandroalbanez.cursomc.services.exception.DataIntegrityException;
 import com.leandroalbanez.cursomc.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -44,6 +47,11 @@ public class CategoriaService {
         } catch (DataIntegrityViolationException e) {
             throw new DataIntegrityException("Não é possível excluir uma categoria com Produtos");
         }
+    }
+
+    public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repository.findAll(pageRequest);
     }
 }
 
