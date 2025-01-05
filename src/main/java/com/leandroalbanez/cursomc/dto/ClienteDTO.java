49380cd85da31ee2006e399a977657f4b12b508a
Leandro Rezende Albanez <leandrorezende89@gmail.com>
2025-01-05 18:07:43 -0300

    PePUT, DELETE e GET para Cliente

---
+++ src/main/java/com/leandroalbanez/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,51 @@
+package com.leandroalbanez.cursomc.dto;
+
+import com.leandroalbanez.cursomc.domain.Cliente;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+
+public class ClienteDTO {
+    private Integer id;
+
+    @NotEmpty(message = "Preenchimento Obrigatório")
+    @Length(min=5, max=120, message = "O tamanho deve ser entre 5 e 120 caracteres")
+    private String nome;
+
+    @NotEmpty(message = "Preenchimento Obrigatório")
+    @Email(message = "E-mail inválido")
+    private String email;
+
+    public ClienteDTO() {
+    }
+
+    public ClienteDTO(Cliente cliente) {
+        this.id = cliente.getId();
+        this.nome = cliente.getNome();
+        this.email = cliente.getEmail();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/com/leandroalbanez/cursomc/services/ClienteService.java
+++ src/main/java/com/leandroalbanez/cursomc/services/ClienteService.java
@@ -1,11 +1,18 @@
 package com.leandroalbanez.cursomc.services;
 
 import com.leandroalbanez.cursomc.domain.Cliente;
+import com.leandroalbanez.cursomc.dto.ClienteDTO;
 import com.leandroalbanez.cursomc.repositories.ClienteRepository;
+import com.leandroalbanez.cursomc.services.exception.DataIntegrityException;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,4 +26,42 @@ public class ClienteService {
         return cliente.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id
                 + " Tipo: " + Cliente.class.getName(),Cliente.class));
     }
+
+    public List<Cliente> findAll() {
+        return repository.findAll();
+    }
+    public Cliente insert(Cliente obj) {
+        obj.setId(null);
+        return repository.save(obj);
+    }
+
+    public Cliente update(Cliente obj) {
+        Cliente newObj = find(obj.getId());
+        updateData(newObj, obj);
+        return repository.save(newObj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir pois há entidades relacionadas");
+        }
+    }
+
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repository.findAll(pageRequest);
+    }
+
+    public Cliente fromDTO(ClienteDTO dto) {
+        return new Cliente(dto.getId(), dto.getNome(), dto.getEmail(), null, null);
+    }
+
+    private void updateData(Cliente newObj, Cliente obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
+    }
+
 }
